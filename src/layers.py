6d36d3b638896f0628f25de6aecd266971755987
annyy <annyyam@yandex.ru>
2026-04-26 22:51:06 +0300

    добавлен и проверен слой dense

--- src/layers.py
+++ src/layers.py
@@ -10,3 +10,30 @@ class ReLULayer:
 
     def backward(self, grad_output):
         return grad_output * (self.input > 0)
+    
+    
+class DenseLayer:
+    def __init__(self, input_size, output_size):
+        # веса и смещения
+        self.weights = np.random.randn(input_size, output_size) * 0.01
+        self.bias = np.zeros((1, output_size))
+        
+        self.input = None
+
+    def forward(self, input):
+        self.input = input
+        return np.dot(input, self.weights) + self.bias
+
+    def backward(self, grad_output, learning_rate):
+        # градиенты
+        grad_weights = np.dot(self.input.T, grad_output)
+        grad_bias = np.sum(grad_output, axis=0, keepdims=True)
+        
+        # градиент для предыдущего слоя
+        grad_input = np.dot(grad_output, self.weights.T)
+        
+        # обновление весов
+        self.weights -= learning_rate * grad_weights
+        self.bias -= learning_rate * grad_bias
+        
+        return grad_input    
